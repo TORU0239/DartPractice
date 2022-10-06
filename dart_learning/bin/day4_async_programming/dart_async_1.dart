64952287ba345c9de2cc0acdffbbc1821df08e0f
Toru Choi <blueocean239@gmail.com>
2022-10-06 23:56:17 +0800

    feat: future with returned value'

--- dart_learning/bin/day4_async_programming/dart_async_1.dart
+++ dart_learning/bin/day4_async_programming/dart_async_1.dart
@@ -1,4 +1,4 @@
-void main() {
+void main() async {
   // Introducing Future
   Future<String> name = Future.value('Toru Choi');
   Future<int> number = Future.value(1);
@@ -10,20 +10,26 @@ void main() {
   // Future.delayed(Duration(seconds: 2), () {
   //   print('Delay end!');
   // });
+  // Only methods returning Future can have await keyword.
 
-  addNumbers(1, 1);
-  addNumbers(2, 2);
+  final result1 = await addNumbers(1, 1);
+  final result2 = await addNumbers(2, 2);
+
+  print('result1: $result1');
+  print('result2: $result1');
+  print('result1 + result2: ${result1 + result2}');
 }
 
 // 1+1 = 2
 // 2+2 = 4
 
 // async and await keyword
-void addNumbers(int number1, int number2) async {
+Future<int> addNumbers(int number1, int number2) async {
   print('calucating: $number1 + $number2');
   // simulating a server
   await Future.delayed(Duration(seconds: 2), () {
     print('calucated: $number1 + $number2 = ${number1 + number2}');
   });
   print('completed! $number1 + $number2');
+  return number1 + number2;
 }
